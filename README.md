5ef720fe3c90800f9cc564178d03f0e71fca6bdd
夜里做梦 <545162831@qq.com>
2019-08-16 09:54:59 +0800

    优化了测试代码和md文件

--- README.md
+++ README.md
@@ -1,9 +1,33 @@
 # PyUtils
-some utils of python.
+**一些Python开发中的实用工具。**
+    
+- Utils
+    - BaseDao.conf
+    - BaseDao.py
+    - CommonSpider.py
+    - ImgSpider.py
+    - MatchUtils.py
 
-* BaseDao.py-------------------访问MySQL的工具，可以实现ORM存取。
-* BaseDao.conf-----------------BaseDao的配置文件
-* CommonSpider.py-----------通用的网页爬取器，可以简单配置就能爬取网页信息，支持翻页。
-* ImgSpider.py-----------------图片爬取器，可以爬取网页中的特定的图片，支持翻页。
-* MatchUtils.py-----------------其他一些文件读取，保存图片之类的小工具。
-* TestUtil.py--------------------测试类
+- Test
+    - model             
+    - BaseDao.conf
+    - Test.py
+    - TestBaseDao.py
+
+| 文件  |  描述  |
+| ----  | ----  |
+| BaseDao.conf | BaseDao的配置文件 |
+| BaseDao.py | 访问MySQL的工具，可以实现ORM存取。 |
+|CommonSpider.py| 通用的网页爬取器，可以简单配置就能爬取网页信息，支持翻页。 |
+|ImgSpider.py| 图片爬取器，可以爬取网页中的特定的图片，支持翻页。 |
+|MatchUtils.py| 其他一些文件读取，保存图片之类的小工具。 |
+
+
+| 文件  |  描述  |
+| ----  | ----  |
+| model | TestBaseDao产生的实体model文件 |
+| BaseDao.conf | TestBaseDao的配置文件 |
+|TestBaseDao.py| <u>**全面测试BaseDao的功能**</u> |
+|Test.py| 测试Util中的其他工具 |
+
+其中，`TestBaseDao.py`是全面测试，也是使用手册，建议先看此文件。
--- Test/Test.py
+++ Test/Test.py
@@ -1,19 +1,9 @@
-from Utils.BaseDao import BaseDao, Page
-
 from Utils.CommonSpider import CommonSpider, Element, Box
 from Utils.ImgSpider import FindImgPages
 from Utils.MatchUtils import MatchUtils
 
 
-class Comment:
-    user_id = None
-    weibo_id = None
-    content = None
-    remark = None
-    time = None
-
-
-def test_CommonSpider():
+def testCommonSpider():
     # 测试CommonSpider
     title = Element()
     title.tag = "span"
@@ -35,7 +25,7 @@ def test_CommonSpider():
 
 
 # 测试ImgSpider
-def test_ImgSpider():
+def testImgSpider():
     fip = FindImgPages()
 
     fip.init("photo-wp", "class", "next_photo", "id", 2)
@@ -44,16 +34,14 @@ def test_ImgSpider():
 
 
 # 测试MatchUtils
-def test_MatchUtils():
-    # MatchUtils.save_img("D:/dd/3/", None, "http://pic.yupoo.com/match7/8152c537/86db77ef.jpg")
-    # MatchUtils.save_text("D:/dd/4/6/hhs.txt", "传说,女娲娘娘炼就七根火柴,来帮助人类度过第一个冬季,从此,七根火柴散落人间,不见踪迹...")
-    # MatchUtils.save_byte("D:/dd/4/5/hhs2.txt", b'1wdd')
+def testMatchUtils():
+    # MatchUtils.save_img("D:/Test/3/", None, "http://pic.yupoo.com/match7/8152c537/86db77ef.jpg")
+    # MatchUtils.save_text("D:/Test/4/6/hhs.txt", "传说,女娲娘娘炼就七根火柴,来帮助人类度过第一个冬季,从此,七根火柴散落人间,不见踪迹...")
+    # MatchUtils.save_byte("D:/Test/4/5/hhs2.txt", b'1wdd')
     content = MatchUtils.web_get("http://www.douban.com")
     print(content)
 
 
-# test_CommonSpider()
-# test_ImgSpider()
-test_MatchUtils()
-
-
+# testCommonSpider()
+# testImgSpider()
+testMatchUtils()
